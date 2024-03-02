b16e69da763618485640aa84b4c90cf499bd0e0f
Arvinder <timsbootcamp@gmail.com>
2024-03-02 18:38:16 +0000

    Added projects

--- my-portfolio/src/components/pages/Projects.jsx
+++ my-portfolio/src/components/pages/Projects.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import projectData from '../../data/projects.json';
 
-function Projects() {
+const Projects = () => {
   return (
-    <div>
-      <h1>Projects Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="container project-gallery">
+      <h2 className="text-center">Project Gallery</h2>
+
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        {projectData.map((project) => (
+          <div key={project.id} className="col mb-4">
+            <Link to={`/project/${project.id}`} className="text-decoration-none">
+              <div className="card h-100 border-0">
+                <img src={project.image} alt={project.title} className="card-img-top" />
+                <div className="card-body">
+                  <h3 className="card-title">{project.title}</h3>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Projects;
